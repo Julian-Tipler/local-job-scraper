f805fd797d82b9df479b5408ab848af8f3dd74da
Julian <tipler.julian@gmail.com>
2024-06-21 09:49:00 -0500

    working

--- supabase/functions/jobs/post/post.ts
+++ supabase/functions/jobs/post/post.ts
@@ -2,17 +2,17 @@ import { supabase } from "../../shared/clients/supabase.ts";
 import PushBullet from "https://esm.sh/pushbullet@3.0.0";
 
 export const post = async (req: Request) => {
-  const payload = await req.json();
+  console.info("******");
+  const { row } = await req.json();
+  const { title } = row;
   const apiKey = Deno.env.get("PUSHBULLET_API_KEY");
   const pusher = new PushBullet(apiKey);
 
-  console.log("PAYLOAD", payload);
   pusher.note(
     "ujwk6Gf85i8sjuVmm2Ra9Y",
     "There are new job(s) available!",
-    `${payload} new job(s) available! \n\n${"hello world"}`,
+    `${title} new job(s) available! \n\n${"link would go here"}`,
   );
-  console.log(req);
 
   return new Response(
     JSON.stringify("hello"),
