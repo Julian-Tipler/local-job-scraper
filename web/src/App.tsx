9e601cd132af7587b2fc256c86e6bd18708b225e
Julian <tipler.julian@gmail.com>
2024-08-07 22:11:26 +0200

    lots of supabase boilerplate

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,17 @@
+import { ExperienceContainer } from "./components/ExperienceContainer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>Experiences: </div>
+      <ExperienceContainer />
+      <ExperienceContainer />
+      <ExperienceContainer />
+    </>
+  );
 }
 
 export default App;
+
+// AKfycbwXfEk9WGUbkEmUdII7YuqFRg1h8QYEFP1rzmTVAIVjZmOlYnL2eWQOr-l-xIZgQEja
+// https://script.google.com/macros/s/AKfycbwXfEk9WGUbkEmUdII7YuqFRg1h8QYEFP1rzmTVAIVjZmOlYnL2eWQOr-l-xIZgQEja/exec
---
+++ web/src/components/ExperienceContainer.tsx
@@ -0,0 +1,3 @@
+export const ExperienceContainer = () => {
+  return <div>ExperienceContainer</div>;
+};
